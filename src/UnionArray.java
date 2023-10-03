b16f7ccb6848c59ee030a8fa1ec074eea3630d69
i-am-sai <saiswaroopkdevarmane@gmail.com>
2023-10-04 00:13:27 +0530

    Day Three

--- src/UnionArray.java
+++ src/UnionArray.java
@@ -1,15 +1,30 @@
 public class UnionArray {
 
     public static int union(int[] arr1, int[] arr2) {
-        int m = arr1.length - 1;
-        int n = arr2.length - 1;
+        int m = arr1.length ;
+        int n = arr2.length ;
         int k = 0;
 
         int[] uni = new int[m + n];
 
-        for (int i = 0; i <= m; i++, k++) {
+        for (int i = 0; i < m; i++, k++) {
             uni[k] = arr1[i];
         }
+
+        for(int i =0; i< n; i++, k++){
+            uni[k] = arr2[i];
+        }
+
+        for(int i =0; i<k; i++){
+            for(int j = i+1; j<k; j++){
+                if(uni[i] == uni[j]){
+                    uni[j] = uni[k-1];
+                    k--;
+                    j--;
+                }
+            }
+        }
+
 
 
         return k;
